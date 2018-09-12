04127d320818f1f8fda4aeb09c3e83e9d7592154
spkelly <spkelly18@gmail.com>
2018-09-12 15:17:00 -0600

    added placeholder pallete

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     Gameboy Tile Generator
+    <div id="pallete">
+      <div class="pallete__color pallete__color-1"></div>
+      <div class="pallete__color pallete__color-2"></div>
+      <div class="pallete__color pallete__color-3"></div>
+      <div class="pallete__color pallete__color-4"></div>
+    </div>
     <div id="grid">
       <div></div>
       <div></div>
@@ -80,6 +86,54 @@ export default {
 </script>
 
 <style>
+
+#pallete{
+  padding: 24px 0;
+  justify-content: space-around;
+  position: relative;
+  display: flex;
+  margin: 20px auto;
+  width: 512px;
+  border-top: 8px solid #113711;
+  border-bottom: 8px solid #113711;
+    background-color: #ccdba4;
+}
+.pallete__color{
+  width: 40px;
+  height: 40px;
+  border: 4px solid  black; 
+  background-color: red;
+}
+
+.pallete__color-1{
+  background-color: #113711;
+}
+.pallete__color-2{
+  background-color: #346031;
+}
+.pallete__color-3{
+  background-color: #8daa27;
+}
+.pallete__color-4{
+  background-color: #9eb929;
+}
+
+#pallete:before, #pallete:after{
+  content: '';
+  top: 0;
+  width: 8px;
+  height: 100%;
+  background: #113711;
+  position: absolute;
+}
+
+#pallete:before{
+  left: -8px;
+}
+
+#pallete:after{
+  right: -8px;
+}
 #app {
   position: absolute;
   
@@ -96,7 +150,7 @@ export default {
 
 #grid{
   position:relative;
-  margin: 120px auto;
+  margin: auto;
   border-top: 8px solid #113711;
   border-bottom: 8px solid #113711;
 
