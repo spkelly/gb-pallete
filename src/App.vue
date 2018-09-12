7291ca9c336b209e5eeeacc4f9fd9becdee152ee
spkelly <spkelly18@gmail.com>
2018-09-12 14:47:39 -0600

    added boilerplate files with placeholders

---
+++ src/App.vue
@@ -0,0 +1,134 @@
+<template>
+  <div id="app">
+    Gameboy Tile Generator
+    <div id="grid">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  position: absolute;
+  
+  text-align: center;
+  bottom: 0px;
+  font-size: 64px;
+  top: 0px;
+  right: 0px;
+  left: 0px;
+  background-color: #9cba29;
+  color: #113711;
+  font-family:'VT323', monospace;
+}
+
+#grid{
+  position:relative;
+  margin: 120px auto;
+  border-top: 8px solid #113711;
+  border-bottom: 8px solid #113711;
+
+  width: 512px;
+  display: flex;
+  flex-wrap: wrap;
+
+}
+
+  #grid:before, #grid:after{
+    content: '';
+    top: 0;
+    width: 8px;
+    height: 100%;
+    background: #113711;
+    position: absolute;
+  }
+
+  #grid:before{
+    left: -8px;
+  }
+
+  #grid:after{
+    right: -8px;
+  }
+
+#grid div{
+  background-color: #ccdba4;
+  height: 64px;
+  box-sizing: border-box;
+  border: 2px dashed white;
+  width: 64px;
+}
+
+</style>
---
+++ src/components/FakeGrid.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    < 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fakegrid'
+}
+</script>
+
+<style>
+#app {
+  position: absolute;
+  text-align: center;
+  bottom: 0px;
+  font-size: 64px;
+  top: 0px;
+  right: 0px;
+  left: 0px;
+  background-color: #9cba29;
+  color: #113711;
+  font-family:'VT323', monospace
+}
+</style>
