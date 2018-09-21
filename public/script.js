7864b356bba20645ab07fdbf7af2e93a1db8ebe9
spkelly <spkelly18@gmail.com>
2018-09-21 14:16:10 -0600

    added basic drawing functionality

--- public/script.js
+++ public/script.js
@@ -1,36 +1,95 @@
 var canvas = document.getElementsByTagName("canvas")[0];
 var ctx = canvas.getContext("2d");
+var isHeld = false;
+var prevCoords = {};
 
-let pixelCount = 8;
+const PIXEL_COUNT = 16;
+const GRID_WIDTH = 400;
+const GRID_HEIGHT = 400;
+const PIXEL_SIZE = GRID_WIDTH/PIXEL_COUNT;
 
-canvas.width = 400;
-canvas.height = 400;
 
-pixelSize = canvas.width/pixelCount;
+canvas.width = GRID_WIDTH;
+canvas.height = GRID_HEIGHT;
+
+
 
 $("canvas").mousedown((e)=>{
-
-  let position = getMousePosition(e);
-  console.table(position);
+  e.preventDefault();
+  isHeld = true
+  checkForDraw(e);
 })
+
+$("canvas").mouseup((e)=>{
+  e.preventDefault();
+  isHeld = false;
+})
+
+$("canvas").mousemove((e)=>{
+  if(isHeld){
+    checkForDraw(e)
+  }
+})
+
+
+
+  //TODO: use this for exporting
+function checkForDraw(e){
+  let position = getMousePosition(e);
+  let coords = getPixelCoordinates(position);
+  if(!_.isEqual(coords,prevCoords)){
+    draw(coords);
+  }
+  prevCoords = coords;
+}
+
+function draw(coords){
+  console.log("Drawing", coords, prevCoords);
+  let offset = getPixelOffset(coords);
+  fillCell(offset);
+}
+
+function fillCell(offset, color){
+  ctx.fillStyle = "#113711";
+  ctx.fillRect(offset.x,offset.y,PIXEL_SIZE,PIXEL_SIZE);
+  ctx.stroke();
+}
 
 function drawGrid(){ 
   ctx.lineWidth = 1;
 
 
-  for(var x = 0; x <= canvas.width; x+= pixelSize){
+  for(var x = 0; x <= canvas.width; x+= PIXEL_SIZE){
     ctx.moveTo(x,0);
     ctx.lineTo(x,canvas.height);
+    ctx.stroke();
   }
 
-  for (let y = 0; y <= canvas.height; y+= pixelSize) {
+  for (let y = 0; y <= canvas.height; y+= PIXEL_SIZE) {
     ctx.moveTo(0, y);
     ctx.lineTo(canvas.width, y);
+    ctx.stroke();
   }
   ctx.stroke();
 
 
 }
+
+function getPixelOffset(coords){
+  return {
+    x:coords.col * PIXEL_SIZE,
+    y:coords.row * PIXEL_SIZE
+  }
+}
+
+function getPixelCoordinates(MousePos){
+  return{
+    col: Math.floor(MousePos.x/PIXEL_SIZE),
+    row: Math.floor(MousePos.y/PIXEL_SIZE)
+  }
+}
+
+
 
 function getMousePosition(e){
   let target = e.target;
@@ -40,5 +99,6 @@ function getMousePosition(e){
     y: e.pageY - target.offsetTop
   }
 }
+
 
 drawGrid();
