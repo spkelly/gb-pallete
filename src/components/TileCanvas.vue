516666a9894437aa74ae099ea2dc7ac660d53a1d
spkelly <spkelly18@gmail.com>
2018-09-28 10:50:33 -0600

    added drawing functions to tile canvas

--- src/components/TileCanvas.vue
+++ src/components/TileCanvas.vue
@@ -1,12 +1,38 @@
 <template>
-  <canvas id="can"></canvas>
+  <canvas id="can" ref="canvas" v-on:mousedown="handleMouseDown"></canvas>
 </template>
 
 <script>
+  import * as CanvasHelper from '../services/CanvasServices'
   export default{
     name: 'TileCanvas',
     props: {
       pixelColor: String
+    },
+    data(){
+      return{
+        canvas:{},
+        ctx:{}
+      }
+    },
+    methods:{
+      handleMouseDown: function(e){
+        let pos = CanvasHelper.getMousePosition(e);
+        let coords = CanvasHelper.getPixelCoordinates(pos);
+        let offset = CanvasHelper.getPixelOffset(coords);
+        this.drawOnCanvas(offset);
+      },
+      handleMouseMove: function(){
+
+      },
+      drawOnCanvas: function(offset){
+        CanvasHelper.fillCell(this.ctx, offset, this.pixelColor);
+      }
+    },
+    mounted() {
+      this.canvas = this.$refs.canvas;
+      this.ctx = this.$refs.canvas.getContext("2d");
+      CanvasHelper.drawGrid(this.canvas,this.ctx);
     }
   }
 </script>
