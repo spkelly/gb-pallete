906b7e1ccb41a5e334b4fdfc0a69fa4d4f43431e
spkelly <spkelly18@gmail.com>
2018-10-05 16:09:29 -0600

    added tile output component to viewraw tile data

---
+++ src/components/TileOutput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="out">
+    <div class="output" >
+      <code v-for="(row, index) in tableData" :key="index">
+        {{row.join()}}
+      </code>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "TileOutput",
+  props:{
+    tableData:Array
+  }
+}
+</script>
+
+<style>
+
+</style>
