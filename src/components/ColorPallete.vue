cc644487fd621ae5aaa940e50a76143f6f9ab964
spkelly <spkelly18@gmail.com>
2018-10-05 16:10:31 -0600

    now emits pallate info with color for table data

--- src/components/ColorPallete.vue
+++ src/components/ColorPallete.vue
@@ -1,14 +1,21 @@
+
 <template>
   <div id="pallete">
-    <div  v-on:click="$emit('changecolor','#113711')" class="pallete__color pallete__color-1"></div>
-    <div  v-on:click="$emit('changecolor','#346031')" class="pallete__color pallete__color-2"></div>
-    <div  v-on:click="$emit('changecolor','#8daa27')" class="pallete__color pallete__color-3"></div>
-    <div  v-on:click="$emit('changecolor','#9eb929')" class="pallete__color pallete__color-4"></div>
+    <div  v-on:click="$emit('changecolor','#113711',3)" class="pallete__color pallete__color-1"></div>
+    <div  v-on:click="$emit('changecolor','#346031',2)" class="pallete__color pallete__color-2"></div>
+    <div  v-on:click="$emit('changecolor','#8daa27',1)" class="pallete__color pallete__color-3"></div>
+    <div  v-on:click="$emit('changecolor','#9eb929',0)" class="pallete__color pallete__color-4"></div>
   </div>
 </template>
 
 <script>
+// TODO: use v-for for this
   export default {
-    name:'colorPallete'
+    name:'colorPallete',
+    data: function(){
+      return{
+        colors: ['#113711','#346031','#8daa27','#9eb929']
+      }
+    }
   }
 </script>
